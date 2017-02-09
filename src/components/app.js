542813d97e2e704701af28045ad3696095db9356
Rob Hitt <robhitt@gmail.com>
2017-02-09 17:19:46 -0500

    add new cocktail form

--- src/components/app.js
+++ src/components/app.js
@@ -2,16 +2,25 @@ import React from 'react';
 import NavBar from './nav_bar';
 import CocktailsIndex from './cocktails_index';
 import CocktailsShow from './cocktails_show';
+import CreateCocktail from './create_cocktail';
 
 export default (props) => {
     return (
       <div>
         < NavBar  title="Boozr" url="/" />
         <div className='container'>
-          <CocktailsIndex />
-          <CocktailsShow />
+          <div className='row'>
+            <div className='col'>
+              <CocktailsIndex />
+            </div>
+          <div className='col-6'>
+            <CocktailsShow />
+          </div>
+          <div className='col'>
+            <CreateCocktail />
+          </div>
         </div>
-
       </div>
+    </div>
     )
 };
